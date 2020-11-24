9cc8f1f51638adf7e072a5f80b9aa623173849a6
khaledhamada <khaledosman737@gmail.com>
2020-11-24 05:44:16 +0200

    assignment_1_1

--- week2/mod3 problems/FCA.sql
+++ week2/mod3 problems/FCA.sql
@@ -9,10 +9,14 @@ DROP TABLE IF EXISTS Facility
 CREATE TABLE Customer(
 	CustNo		varchar(8)			not null,
 	CustName	varchar(50)			not null,
-	Address		varchar(50)			not null,
-	Internal	char(1)				not null,
-	phone		integer				not null,
-	Contact		varchar
+	CustAddress		varchar(50)			not null,
+	CustInternal	char(1)				not null,
+	Custphone		integer				not null,
+	CustContact		varchar(50)			not null,
+	CustCity		varchar(50)			not null,
+	CustState		varchar(24)			not null,
+	CustZip			integer				not null,
+	CONSTANT		CustomerPK PRIMARY KEY(CustNo)
 
 
 
@@ -24,6 +28,33 @@ CREATE TABLE Facility(
 	FacNo		varchar(6)			not null,
 	FacName		varchar(50)		not null,
 	
+	CONSTRAINTS FacNameUn UNIQUE FacName,
 	CONSTANT FacilityPK   PRIMARY KEY(FacNo)
 
 );
+
+-------------------------- Location Table Definition ---------------
+CREATE TABLE Location(
+	
+	LocNo		varchar(6)			not null,
+	FacNo		varchar(6)			not null,
+	LocName		varchar(50)			not null,
+	
+	CONSTANT LocationPk PRIMARY KEY(LocNo),
+	CONSTRAINTS FacNoFK	FOREIGN KEY(FacNo) REFERENCES Facility,
+	
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
