4c9965a39ecb92fb9910d3f2154d9ce279a98432
khaledhamada <khaledosman737@gmail.com>
2020-12-02 07:13:23 +0200

    assignment 2

--- week2/mod3 problems/FCA.sql
+++ week2/mod3 problems/FCA.sql
@@ -1,6 +1,11 @@
-DROP TABLE IF EXISTS Customer
-DROP TABLE IF EXISTS Facility 
-
+DROP TABLE IF EXISTS Customer;
+DROP TABLE IF EXISTS Facility ;
+DROP TABLE IF EXISTS Location ;
+DROP TABLE IF EXISTS Employee ;
+DROP TABLE IF EXISTS ResourceTbl; 
+DROP TABLE IF EXISTS EventRequest; 
+DROP TABLE IF EXISTS EventPlan ;
+DROP TABLE IF EXISTS EventPlanLine; 
 
 
 
@@ -9,14 +14,14 @@ DROP TABLE IF EXISTS Facility
 CREATE TABLE Customer(
 	CustNo		varchar(8)			not null,
 	CustName	varchar(50)			not null,
-	CustAddress		varchar(50)			not null,
-	CustInternal	char(1)				not null,
-	Custphone		integer				not null,
-	CustContact		varchar(50)			not null,
-	CustCity		varchar(50)			not null,
-	CustState		varchar(24)			not null,
-	CustZip			integer				not null,
-	CONSTANT		CustomerPK PRIMARY KEY(CustNo)
+	Address		varchar(50)			not null,
+	Internal	char(1)				not null,
+	phone		integer				not null,
+	Contact		varchar(50)			not null,
+	City		varchar(50)			not null,
+	State		varchar(24)			not null,
+	Zip			integer				not null,
+	CONSTRAINT		CustomerPK PRIMARY KEY(CustNo)
 
 
 
@@ -25,36 +30,106 @@ CREATE TABLE Customer(
 
 ------------------- Facility Table Definition ----------------
 CREATE TABLE Facility(
-	FacNo		varchar(6)			not null,
+	FacNo		varchar(8)			not null,
 	FacName		varchar(50)		not null,
 	
-	CONSTRAINTS FacNameUn UNIQUE FacName,
-	CONSTANT FacilityPK   PRIMARY KEY(FacNo)
+	CONSTRAINT FacNameUn UNIQUE (FacName),
+	CONSTRAINT FacilityPK   PRIMARY KEY(FacNo)
 
 );
 
 -------------------------- Location Table Definition ---------------
 CREATE TABLE Location(
 	
-	LocNo		varchar(6)			not null,
-	FacNo		varchar(6)			not null,
+	LocNo		varchar(8)			not null,
+	FacNo		varchar(8)			not null,
 	LocName		varchar(50)			not null,
 	
-	CONSTANT LocationPk PRIMARY KEY(LocNo),
-	CONSTRAINTS FacNoFK	FOREIGN KEY(FacNo) REFERENCES Facility,
+	CONSTRAINT LocationPk PRIMARY KEY(LocNo),
+	CONSTRAINT FacNoFK	FOREIGN KEY(FacNo) REFERENCES Facility ON DELETE CASCADE
 	
 );
 
+------ Employee Table --------------------------
+CREATE TABLE Employee(
+	EmpNo			varchar(8)			not null,
+	EmpName			varchar(50)			not null,
+	Department		varchar(50)			not null,
+	Email			varchar(50)			not null,
+	Phone			varchar(8)			not null,
+	
+	CONSTRAINT 		EmployeePK 	PRIMARY KEY(EmpNo)
 
 
+);
 
 
+-------------------------ResourceTbl  Table --------------------------
+CREATE TABLE ResourceTbl(
+	ResNo			varchar(8)			not null,
+	ResName			varchar(50)			not null,
+	Rate			decimal(5,2)				not null,
+	
+	CONSTRAINT		ResourceTblPK		PRIMARY KEY(ResNo),
+	CONSTRAINT		ResourceRateValue	CHECK( Rate > 0)
 
 
+);
+
+------------------------- Event Request Table ---------------
+CREATE TABLE EventRequest(
+	EventNo			varchar(8)			not null,
+	DateHeld		date				not null,
+	DateReq			date				not null,
+	FacNo			varchar(8)			not null,
+	CustNo			varchar(8)			not null,
+	DateAuth		date				,
+	Status			varchar(8)			not null,
+	EstCost			decimal(8,2)				not null,
+	EstAudience		integer				not null,
+	BudNo			varchar(8)			,
+	
+	CONSTRAINT		EventRequestPK		PRIMARY KEY(EventNo),
+	CONSTRAINT		CustomerFK			FOREIGN KEY(CustNo) REFERENCES Customer ON DELETE CASCADE,
+	CONSTRAINT		FacilityFK			FOREIGN KEY(FacNo) REFERENCES Facility ON DELETE CASCADE,
+	CONSTRAINT		EventStatus			CHECK (Status IN ('Pending','Denied','Approved')),
+	CONSTRAINT		EventRequestEstAudienceValue	CHECK( EstAudience > 0)
+);
+
+----------Event	Plan Table--------
+CREATE TABLE EventPlan(
+	PlanNo			varchar(8)			not null,
+	EventNo			varchar(8)			not null,
+	Workdate		date				not null,
+	Notes			varchar(50)			,
+	Activity		varchar(50)			not null,
+	EmpNo			varchar(8)			,
+	
+	CONSTRAINT	EventPlanPK PRIMARY KEY(PlanNo),
+	
+	CONSTRAINT	EventFK		FOREIGN KEY(EventNo)
+	REFERENCES	EventRequest ON DELETE CASCADE,
+	
+	CONSTRAINT	EmployeeFK	FOREIGN KEY(EmpNo)
+	REFERENCES Employee ON DELETE CASCADE
 
 
+);
+-----------------------Event Plan Line Table-----
+CREATE TABLE EventPlanLine(
+	PlanNo			varchar(8)			not null,
+	LineNo			integer				not null,
+	TimeStart		timestamp			not null,
+	TimeEnd			timestamp			not null,
+	NumberFld		integer				not null,
+	LocNo			varchar(8)			not null,
+	ResNo			varchar(8)			not null,
+	
+	CONSTRAINT		EventPlanLinePK		PRIMARY KEY(PlanNo,LineNo),
+	CONSTRAINT		PlanFK				FOREIGN KEY(PlanNo)		REFERENCES EventPlan ON DELETE CASCADE,
+	CONSTRAINT		LocationFK				FOREIGN KEY(LocNo)		REFERENCES Location ON DELETE CASCADE,
+	CONSTRAINT		RessourceTblFK				FOREIGN KEY(ResNo)		REFERENCES ResourceTbl ON DELETE CASCADE,
+	CONSTRAINT		ValidStartEndTime		CHECK(TimeStart < TimeEnd)
 
 
-
-
-
+);
